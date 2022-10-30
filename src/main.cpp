a4f272d0ab297bc131d8864713f29075e5cbfe9a
Monika <monikamajchrzak94@gmail.com>
2022-10-30 19:12:38 +0100

    Usuniecie wykrzyknika

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
   if (argc <2) {
 
-    std::cout << "bez parametrow!!!" << std::endl;
+    std::cout << "bez parametrow!" << std::endl;
 
   } else {
     std::cout << pole ((std::stod(argv[1])), (std::stod(argv[2])))<< std::endl;
